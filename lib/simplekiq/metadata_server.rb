8ddd3a9c8edac7fdb2d239e10a2deec1d398d4e8
Aaron Bernard <abernard@chime.com>
2019-05-01 11:13:37 -0500

    SCALE-248: add code to describe where code is enqueued from

--- lib/simplekiq/metadata_server.rb
+++ lib/simplekiq/metadata_server.rb
@@ -51,7 +51,43 @@ module Simplekiq
     end
 
     def enqueued_from_src_location
-      nil
+      last_stack_is_simplekiq = false
+      caller.inspect.reverse_each do |callstack_line|
+        entry = split_stackline(callstack_line)
+        if is_simplekiq_callstack?(entry)
+          last_stack_is_simplekiq = true
+        else
+          if last_stack_is_simplekiq
+            return callstack_entry_hash(entry)
+          end
+          last_stack_is_simplekiq = false
+        end
+      end
+
+      callstack_entry_hash([nil, nil, nil])
+    end
+
+    private
+
+    def is_simplekiq_callstack?(callstack_entry)
+      callstack_entry.first.include?('simplekiq') || callstack_entry.first.include?('sidekiq')
+    end
+
+    def callstack_entry_hash(callstack_entry)
+      {
+        path: callstack_entry[0],
+        line: callstack_entry[1],
+        method: callstack_entry[2]
+      }
+    end
+
+    def split_stackline(callstack_line)
+      split = callstack_line.rpartition(':in ')
+      method = split.last.tr('`', '')
+      split_2 = split.first.rpartition(':')
+      path = split_2.first
+      line = split_2.last
+      [path, line, method].freeze
     end
   end
 end
