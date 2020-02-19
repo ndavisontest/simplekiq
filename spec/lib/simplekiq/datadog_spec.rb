580f0b749bd5d8377d09c1c2fcfe19098914e540
Matt Taylor <mtaylor@chime.com>
2020-02-19 13:05:57 -0800

    Fix v4.0.1

--- spec/lib/simplekiq/datadog_spec.rb
+++ spec/lib/simplekiq/datadog_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 RSpec.describe Simplekiq::Datadog do
   context 'tags' do
+    subject(:tag) do
+      datadog_args[:tags][tag_number].call(nil, nil, nil, nil)
+    end
+    let(:tag_number) { 0 }
     let(:datadog_middleware) do
       Sidekiq.server_middleware.entries.detect do |entry|
         entry.klass == Sidekiq::Middleware::Server::Datadog
@@ -11,29 +15,39 @@ RSpec.describe Simplekiq::Datadog do
     let(:datadog_args) do
       datadog_middleware.instance_variable_get(:@args).first
     end
+    context 'when checking first tag' do
+      it { expect(subject).to eq('service:undefined') }
 
-    subject(:tag) do
-      datadog_args[:tags].first.call(nil, nil, nil, nil)
+      context 'when Rails application' do
+        let(:rails) { class_double('Rails') }
+        before do
+          stub_const('Rails', rails)
+          stub_const('Rails::VERSION::MAJOR', 6)
+          allow(rails).to receive_message_chain(
+            'application.class.module_parent_name.underscore'
+          ).and_return('app_name')
+        end
+
+        it { expect(subject).to eq('service:app_name') }
+      end
     end
+    context 'when checking second tag' do
+      let(:tag_number) { 1 }
 
-    it { expect(subject).to eq('service:undefined,worker_group:undefined') }
-
-    context 'when Rails application' do
-      let(:rails) { class_double('Rails') }
-      before do
-        stub_const('Rails', rails)
-        stub_const('Rails::VERSION::MAJOR', 6)
-        allow(rails).to receive_message_chain(
-          'application.class.module_parent_name.underscore'
-        ).and_return('app_name')
-        stub_const(
-          'ENV',
-          ENV.to_hash.merge('DATADOG_SIDEKIQ_WORKER_GROUP' => 'custom_worker_group')
-        )
+      context 'when DATADOG_SIDEKIQ_WORKER_GROUP env variable not set' do
+        it { expect(subject).to eq('worker_group:undefined') }
       end
 
-      it do
-        expect(subject).to eq('service:app_name,worker_group:custom_worker_group')
+      context 'when DATADOG_SIDEKIQ_WORKER_GROUP env variable set' do
+        before do
+          stub_const(
+            'ENV',
+            ENV.to_hash.merge(
+              'DATADOG_SIDEKIQ_WORKER_GROUP' => 'custom_worker_group'
+            )
+          )
+        end
+        it { expect(subject).to eq('worker_group:custom_worker_group') }
       end
     end
   end
